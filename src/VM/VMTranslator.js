4c329158d38922229cea53b4fd0d24d1816948cc
Cycoder <mnerren@gmail.com>
2024-01-04 20:28:26 -0800

    implement basic pop

--- src/VM/VMTranslator.js
+++ src/VM/VMTranslator.js
@@ -5,9 +5,13 @@ const { parseArgs } = require("node:util");
 
 class VMTranslator {
   constructor(file) {
+    this.rootName = path.basename(file, ".vm");
     this.eqJMPNum = 0;
     this.gtJMPNum = 0;
     this.ltJMPNum = 0;
+    this.staticNum = 0;
+    this.rA = "__temp_register_A";
+    this.rB = "__temp_register_B";
     this.symbolMap = {
       LOCAL: "LCL",
       ARGUMENT: "ARG",
@@ -24,16 +28,25 @@ class VMTranslator {
     this.ast = this.processCommands(this.stripInput(this.rawFile));
     this.assembly = this.translate(this.ast);
     console.log("AST:", this.ast);
+    console.log("ASSEMBLY:", this.assembly);
   }
 
   translate(ast) {
     const assembly = [];
-    for (let command of ast) {
+    for (let item of ast) {
+      switch (item.command) {
+        case "pop":
+          assembly.push(this.pop(item.register, item.offset, item.source));
+          break;
+
+        default:
+          break;
+      }
     }
     return assembly;
   }
 
-  pop(register, offset) {
+  pop(register, offset, source) {
     const assembly = [];
     switch (register) {
       case "LCL":
@@ -43,6 +56,23 @@ class VMTranslator {
       case "R13":
       case "R14":
       case "R15":
+        assembly.push(`// ${source}`);
+        assembly.push(`@SP`);
+        assembly.push(`M=M-1`);
+        assembly.push(`@${register}`);
+        assembly.push(`D=A`);
+        assembly.push(`@${offset}`);
+        assembly.push(`D=D+A`);
+        assembly.push(`@${this.rA}`);
+        assembly.push(`M=D`);
+        assembly.push(`@SP`);
+        assembly.push(`D=M`);
+        assembly.push(`@${this.rA}`);
+        assembly.push(`A=M`);
+        assembly.push(`M=D`);
+        break;
+      case "STATIC":
+        // IMPLEMENT STATIC
         break;
       case "TEMP":
         // IMPLEMENT TEMP
@@ -53,6 +83,7 @@ class VMTranslator {
       default:
         break;
     }
+    return assembly;
   }
 
   processCommands(input) {
@@ -66,7 +97,7 @@ class VMTranslator {
           {
             let [register, offset] = rest;
             register = this.symbolMap[register.toUpperCase()];
-            const obj = { command, register, offset };
+            const obj = { command, register, offset, source: item };
             ast.push(obj);
           }
           break;
@@ -80,7 +111,7 @@ class VMTranslator {
         case "or":
         case "not":
           {
-            const obj = { command };
+            const obj = { command, source: item };
             ast.push(obj);
           }
           break;
